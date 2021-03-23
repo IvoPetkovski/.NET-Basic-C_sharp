db5215777d389eed2e022a22d91f867e552e3050
Ivan Petkovski <petkovski.ivo@gmail.com>
2021-03-23 21:35:39 +0100

    Homework01 Traffic lights

---
+++ Homework_01/Homework01/Homework01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Homework01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("STOP!");
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine("READY");
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("GO!");
+            Console.ReadLine();
+        }
+    }
+}
