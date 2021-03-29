25f9837f86707b7d5813bcf73dee0791472d24e7
Ivan Petkovski <petkovski.ivo@gmail.com>
2021-03-29 22:46:36 +0200

    Homework 02 - 3 Tasks

---
+++ Homework_02/Homework_02_Task01/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace Homework_02_Task01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number1;
+
+            Console.Write("Please enter the first number: ");
+            string numberInput1 = Console.ReadLine();
+
+            bool checkInput1 = double.TryParse(numberInput1, out number1);
+
+            while (!checkInput1)
+            {
+                Console.Write("Please enter the first number with numbers: ");
+                numberInput1 = Console.ReadLine();
+                checkInput1 = double.TryParse(numberInput1, out number1);
+            }
+
+
+            double number2;
+
+            Console.Write("Please enter the second number: ");
+            string numberInput2 = Console.ReadLine();
+
+
+            bool checkInput2 = double.TryParse(numberInput2, out number2);
+
+            while (!checkInput2)
+            {
+                Console.Write("Please enter the second number with numbers: ");
+                numberInput2 = Console.ReadLine();
+                checkInput2 = double.TryParse(numberInput2, out number2);
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("The first number you have entered is: " + number1);
+            Console.WriteLine("The second number you have entered is: " + number2);
+
+            Console.WriteLine();
+
+            Console.Write("Please enter the operation you want to be executed: ");
+            string operation = Console.ReadLine();
+
+            while (operation != "+" && operation != "-" && operation != "*" && operation != "/")
+            {
+                Console.Write("Please enter a valid operation: ");
+                operation = Console.ReadLine();
+            }
+
+            Console.WriteLine();
+
+            if (operation == "+")
+            {
+                double result = number1 + number2;
+                Console.WriteLine($"The addition of the two numbers {number1} and {number2} is {result}");
+            }
+            else if (operation == "-")
+            {
+                double result = number1 - number2;
+                Console.WriteLine($"The subtraction of the two numbers {number1} and {number2} is {result}");
+            }
+            else if (operation == "*")
+            {
+                double result = number1 * number2;
+                Console.WriteLine($"The multiplication of the two numbers {number1} and {number2} is {result}");
+            }
+            else
+            {
+                double result = number1 / number2;
+                Console.WriteLine($"The division of the two numbers {number1} and {number2} is {result}");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Homework_02/Homework_02_Task02/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Homework_02_Task02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the first number: ");
+            string numberInput1 = Console.ReadLine();
+
+            Console.Write("Enter the second number: ");
+            string numberInput2 = Console.ReadLine();
+
+            Console.Write("Enter the third number: ");
+            string numberInput3 = Console.ReadLine();
+
+            Console.Write("Enter the fourth number: ");
+            string numberInput4 = Console.ReadLine();
+
+            double result = (double.Parse(numberInput1) + double.Parse(numberInput2) + double.Parse(numberInput3) + double.Parse(numberInput4)) / 4;
+            Console.WriteLine($"The average of {numberInput1}, {numberInput2}, {numberInput3} and {numberInput4} is: {result}");
+
+            Console.ReadLine();
+        }
+    }
+}
