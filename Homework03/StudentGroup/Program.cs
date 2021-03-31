3c3bf09e714f61e5ba512d58706637af614a3bf3
Ivan Petkovski <petkovski.ivo@gmail.com>
2021-03-31 22:58:55 +0200

    Homework03 SumOfEven/StudentGroup

---
+++ Homework03/StudentGroup/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace StudentGroup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] studentsG1 = new string[] { "Ivan", "Angela", "Petar", "Sofija", "Aleksandar" };
+            string[] studentsG5 = new string[] { "Marjan", "Emilija", "Bojan", "Teodora", "Anita" };
+            int num;
+
+
+            Console.Write("Please enter the student group with 1 or 5 (G1 or G5): ");
+            string numberInput = Console.ReadLine();
+            bool checkInput = int.TryParse(numberInput, out num);
+
+            while (!checkInput || (num != 1 && num != 5))
+            {
+                Console.Write("Please enter 1 or 5 with NUMBERS to see the names of the Groups: ");
+                numberInput = Console.ReadLine();
+                checkInput = int.TryParse(numberInput, out num);
+            }
+            if (num == 1)
+            {
+                Console.WriteLine("The Students in G1 are:");
+                for (int i = 0; i < studentsG1.Length; i++)
+                {
+                    Console.WriteLine($"{i + 1}. {studentsG1[i]}");
+                }
+            }
+            else if (num == 5)
+            {
+                Console.WriteLine("The Students in G5 are:");
+                for (int i = 0; i < studentsG5.Length; i++)
+                { 
+                    Console.WriteLine($"{i + 1}. {studentsG5[i]}");
+                }
+            }
+        }
+    }
+}
---
+++ Homework03/SumOfEven/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SumOfEven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numInput = new int[6];
+            int sumOfEven = 0;
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.Write($"Enter the {i + 1} number: ");
+                numInput[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < numInput.Length; i++)
+            {
+                if (numInput[i] % 2 == 0)
+                {
+                    sumOfEven = sumOfEven + numInput[i];
+                }
+
+            }
+            Console.WriteLine($"The sum of the even numbers is: {sumOfEven}");
+
+
+            Console.ReadLine();
+        }
+    }
+}
